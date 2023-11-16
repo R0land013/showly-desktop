e461cdda08056a0340945f72934075f6dc26cac9
Rolando Rio <58601896+R0land013@users.noreply.github.com>
2023-11-16 16:18:50 +0100

    Init project

---
+++ src/main/java/io/github/r0land013/SystemInfo.java
@@ -0,0 +1,13 @@
+package io.github.r0land013;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module io.github.r0land013 {
+    requires javafx.controls;
+    exports io.github.r0land013;
+}
