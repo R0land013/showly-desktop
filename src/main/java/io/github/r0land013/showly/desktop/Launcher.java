9521c5f37f16fdecf1acd296616a4bc91f0fb3d2
Rolando Rio <58601896+R0land013@users.noreply.github.com>
2024-07-20 11:44:56 +0200

    Build: Created github Workflow to package app as jar file being supported on Linux and Windows

---
+++ src/main/java/io/github/r0land013/showly/desktop/Launcher.java
@@ -0,0 +1,9 @@
+package io.github.r0land013.showly.desktop;
+import io.github.r0land013.showly.desktop.App;
+
+public class Launcher {
+    
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
